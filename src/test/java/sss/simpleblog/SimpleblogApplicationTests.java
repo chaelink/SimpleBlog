e728aed111056525abd773b7fe30b05a1807f66d
kikuke <kkkjs0814@naver.com>
2023-06-21 16:54:08 +0900

    [Add] init

---
+++ src/test/java/sss/simpleblog/SimpleblogApplicationTests.java
@@ -0,0 +1,13 @@
+package sss.simpleblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
